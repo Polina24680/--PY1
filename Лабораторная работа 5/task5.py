317ac6d26d6ddfeeea2c41a5aa785c397fdd55f1
Polina24680 <115586932+Polina24680@users.noreply.github.com>
2022-11-16 23:42:09 +0300

    Add files via upload

---
+++ Лабораторная работа 5/task5.py
@@ -0,0 +1,14 @@
+def get_random_password() -> str:
+    ...  # TODO написать функцию генерации случайных паролей
+
+
+
+import random
+
+
+def get_random_password(n=8):
+    symbols_str = "ASDFGHJKLZXCVBNMQWERTYUIOPasdfghjklpqowieurytzxcvbnm0123456789"
+    return random.sample(symbols_str, n)
+
+
+print(get_random_password())
