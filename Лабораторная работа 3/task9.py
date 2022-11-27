f6ecf98da1c122dc4b00d64ee1ba4cd2e34df81d
Polina24680 <115586932+Polina24680@users.noreply.github.com>
2022-11-27 23:17:26 +0300

    Update task9.py

--- Лабораторная работа 3/task9.py
+++ Лабораторная работа 3/task9.py
@@ -4,11 +4,13 @@ months = 10  # количество месяцев
 increase = 0.03  # рост цен
 
 need_money = 0  # количество денег, чтобы прожить 10 месяцев
-
+delta_need = spend - salary
+need = 0
 # TODO Оформить решение
+for i in tuple(range(1,10)):
+    spend = spend * (increase+1)
+    need = need + spend - salary
 
-
-
-
+need_money = delta_need + need
 
 print(round(need_money))
